19ed5d6f3fabc8c5c24262a1d7f4fa1bb08b4bf2
nikitaolenych123-coder <nikitaolenych123@gmail.com>
2025-12-26 13:46:27 +0000

    chore: add minimal app sources and resources so app module compiles

---
+++ app/src/main/java/com/protonhorizon/emulator/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package com.protonhorizon.emulator
+
+import android.app.Activity
+import android.os.Bundle
+
+class SettingsActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // placeholder
+    }
+}
